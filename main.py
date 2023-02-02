7888b4818e8831b9243f0bda1ffeccd2622e6901
abh <themechanic002@gmail.com>
2023-02-02 20:31:50 +0900

    Add: First commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+
+# Headers는 'Content-Type': 'application/json'
+# Body는 JSON 형식으로 요청
+
+def recommendation():
+    if request.method == "POST":
+        data = request.get_json()
+        isbn = data.get("isbn")
+        if isbn:
+            return jsonify({"message": "Success! Request method is POST!"}), 201
+        else:
+            return jsonify({"message": "Error"}), 400
+    else:
+        return jsonify({"message": "Success! Request method is GET!"}), 200
+
+if __name__ == "__main__":
+    app.run(host = '127.0.0.1', port=8080)
